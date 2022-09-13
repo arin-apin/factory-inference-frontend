54992a91002e6df973531bd113c925ef8a9d7bb3
mario <mario@arinapin.ai>
2022-09-13 17:51:42 +0200

    combrocacion añadir fichero

---
+++ comprobacionjpg.py
@@ -0,0 +1,41 @@
+#Programa para comprobar que los fichero son jpg. y comprobación de categorías OK NOOK
+
+
+import fnmatch
+import os
+
+flag1 = 0
+flag2 = 0
+
+print(os.listdir('.'))
+for file in os.listdir('.'):
+    if fnmatch.fnmatch(file, 'OK'):
+        print("OK")
+        flag1 = 1
+    if fnmatch.fnmatch(file, 'NO OK'):
+        print("NO OK")
+        flag2 = 1
+    
+
+if flag1 == 1 and flag2 == 1:
+    print("Ficheros encontrados, proceder")
+    
+
+else:
+        print("missing folder with correct names, OK or NO OK")
+        exit()
+
+
+#Comprobar que las imagenes tienen el formato adecuado 
+for root, dirs, files in os.walk("./NO OK"):
+    print(os.listdir('./NO OK'))
+    # if fnmatch.fnmatch(file, '*.txt'):
+    #     print(file)
+
+for root, dirs, files in os.walk("./OK"):
+    print(files)
+    for file in files: 
+        if file.endswith(".jpg"):
+            print("correcto")
+        else:
+            print("error")
